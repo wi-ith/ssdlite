227022d103a3c18b3edc48b4eda062cd066d069e
DESKTOP-9EPMHDO\RayKwak <kdgil3415@naver.com>
2020-02-12 02:02:07 +0900

    train.py

--- ssd_lite/train.py
+++ ssd_lite/train.py
@@ -35,40 +35,6 @@ tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
 
 
-def tower_loss(scope, images, labels):
-    """Calculate the total loss on a single tower running the CIFAR model.
-    Args:
-      scope: unique prefix string identifying the CIFAR tower, e.g. 'tower_0'
-      images: Images. 4D tensor of shape [batch_size, height, width, 3].
-      labels: Labels. 1D tensor of shape [batch_size].
-    Returns:
-       Tensor of shape [] containing the total loss for a batch of data
-    """
-
-    # Build inference Graph.
-    logits = cifar10.inference(images)
-
-    # Build the portion of the Graph calculating the losses. Note that we will
-    # assemble the total_loss using a custom function below.
-    _ = cifar10.loss(logits, labels)
-
-    # Assemble all of the losses for the current tower only.
-    losses = tf.get_collection('losses', scope)
-
-    # Calculate the total loss for the current tower.
-    total_loss = tf.add_n(losses, name='total_loss')
-
-    # Attach a scalar summary to all individual losses and the total loss; do the
-    # same for the averaged version of the losses.
-    for l in losses + [total_loss]:
-        # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
-        # session. This helps the clarity of presentation on tensorboard.
-        loss_name = re.sub('%s_[0-9]*/' % cifar10.TOWER_NAME, '', l.op.name)
-        tf.summary.scalar(loss_name, l)
-
-    return total_loss
-
-
 def average_gradients(tower_grads):
     """Calculate the average gradient for each shared variable across all towers.
     Note that this function provides a synchronization point across all towers.
