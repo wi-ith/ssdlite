ba10f5c91b3fa23f6005ddbdc15eb7439c213042
DESKTOP-9EPMHDO\RayKwak <kdgil3415@naver.com>
2020-02-18 02:23:18 +0900

    small change

--- ssd_lite/anchor.py
+++ ssd_lite/anchor.py
@@ -14,12 +14,14 @@ scale_list=(scale_range-1)*((max_scale-min_scale)/5.)+min_scale
 scale_list=tf.concat([[0.1],scale_list],axis=0)
 
 
+
 for feature,ratio,scale in zip(feature_maps,ratio_list,scale_list[:-1]):
     feature.get_shape().as_list()[1:3]
 
 import tensorflow as tf
 import numpy as np
-feature=tf.ones([2,19,19,960])
+
+feature=tf.ones([2,3,3,256])
 anchor_size=feature.get_shape().as_list()[1:3]
 ratio=(2,1,1/2)
 scale=0.1
@@ -35,28 +37,19 @@ row_coordi=tf.tile(row_coordi,[anchor_size[0],1])
 col_coordi=tf.reshape(col_coordi,[-1,anchor_size[0]])
 row_coordi=tf.reshape(row_coordi,[-1,anchor_size[0]])
 
-anchor_x1y1=tf.stack([col_coordi,row_coordi],axis=2)
+anchor_x1y1=tf.stack([row_coordi,col_coordi],axis=2)
 anchor_x2y2=anchor_x1y1+1
 
+anchor_x1y1[:,:,0] = anchor_x1y1[:,:,0]+(1./2.)*(tf.math.sqrt(scale) - 1.)
+anchor_x1y1[:,:,1] = anchor_x1y1[:,:,1]+(1./2.)*(1. - 1/tf.math.sqrt(scale))
+anchor_x2y2[:,:,0] = anchor_x2y2[:,:,0]-(1./2.)*(tf.math.sqrt(scale) - 1.)
+anchor_x2y2[:,:,1] = anchor_x2y2[:,:,1]-(1./2.)*(1. - 1/tf.math.sqrt(scale))
+#num_scale x 19 x 19 x 4
+
 
 sess=tf.Session()
-sess.run(anchor_x2y2)
+sess.run(anchor_x1y1)
 
+import subprocess
 
-
-
-tmp_col=np.reshape(tmp,[6,1])
-tmp_row=np.reshape(tmp,[1,6])
-col_=tf.tile(tmp_col,[1,6])
-col_=tf.reshape(col_,[-1,6])
-row_=tf.tile(tmp_col,[6,1])
-row_=tf.reshape(row_,[-1,6])
-anchor=tf.stack([col_,row_],axis=2)
-
-sess=tf.Session()
-sess.run(coordi_range)
-
-a,b,c=sess.run([col_,row_,anchor])
-print(a)
-print(b)
-print(c)
+subprocess.call ('pip3 install tensorflow==1.13.1', shell=True)
