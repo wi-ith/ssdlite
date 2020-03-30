938f004c8a7b3bb4ac3fbec5076af6c3741c6a88
wi-ith <59470424+wi-ith@users.noreply.github.com>
2020-03-31 00:33:39 +0900

    Update train.sh

--- ssd_lite/train.sh
+++ ssd_lite/train.sh
@@ -3,7 +3,7 @@ python3 train.py \
   --ckpt_save_path=./ckpt/0321/ \
   --pretrained_ckpt_path=../pretrained/ssdlite_mobilenet/model.ckpt \
   --mode=train \
-  --tfrecords_dir=/home/kdg/tfrecords/google_mscoco/32k \
+  --tfrecords_dir=./dir/to/tfrecords \
   --image_size=300 \
   --max_boxes=120 \
   --num_train=32147 \
